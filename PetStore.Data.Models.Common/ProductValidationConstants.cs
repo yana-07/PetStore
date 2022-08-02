5c35e01c40e16756b76a3c88d6f5f327dd306d71
yana.radeva7 <yana.radeva7@gmail.com>
2022-08-02 19:09:53 +0300

    add constants to be used for validating the input model when creating a new product

--- PetStore.Data.Models.Common/ProductValidationConstants.cs
+++ PetStore.Data.Models.Common/ProductValidationConstants.cs
@@ -3,5 +3,13 @@
     public static class ProductValidationConstants
     {
         public const int NameMaxLength = 70;
+        public const int NameMinLength = 3;
+
+        public const string NameIsRequiredMessage = "Product name is required!";
+        public const string NameMinLengthMessage = $"Product name must be at least 3 characters long!";
+        public const string NameMaxLengthMessage = "Product name must be no more than 70 characters long!";
+
+        public const string PriceMinValue = "0";
+        public const string PriceMaxValue = "79228162514264337593543950335";
     }
 }
